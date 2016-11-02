a06ecb43c09bfa038c58c60a646b1f2266b889e3
ninjaviallage <mazedur@gmail.com>
2016-11-02 19:39:17 +0600

    initial commit

---
+++ Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApp
+{     
+
+
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("constructing animal");
+        }
+        public Animal(GenderType gender)
+        {
+            if (gender == GenderType.Female)
+            {
+                Console.WriteLine("Female");
+            }
+            else
+            {
+                Console.WriteLine("Male");
+            }
+        }
+    }
+    public class Elephant : Animal
+    {
+        public Elephant(GenderType gender)
+            : base(gender)
+        {
+            Console.WriteLine("Elephant");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant e = new Elephant(GenderType.Female);
+            Console.ReadKey();
+        }
+    }
+}
